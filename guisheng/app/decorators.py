37fe0d215061ed2879756c58381c9f87d13e6bb4
roseou <598959770@qq.com>
2016-12-06 20:35:27 +0800

    edit decorators.py

--- guisheng/app/decorators.py
+++ guisheng/app/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import abort, request, g
+from flask import abort, request, g, jsonify
 from flask_login import current_user
 from app.models import User
 import base64
@@ -9,14 +9,15 @@ def admin_required(f):
     def decorated(*args,**kwargs):
         token_header = request.headers.get('Authorization',None)
         if token_header:
-            token_hash = token_header.split()[1]
-            token = base64.b64decode(token_hash)[:-1]
+            token_hash = token_header[6:]
+            decode_token = base64.b64decode(token_hash)
+            token = decode_token[:-1]
             g.current_user = User.verify_auth_token(token)
             if not g.current_user.is_administrator():
-                abort(403)
+                return jsonify({'message': '403 Forbidden'}), 403
             return f(*args,**kwargs)
         else:
-            abort(401)
+            return jsonify({'message': '401 unAuthorization'}), 401
     return decorated
 
 
