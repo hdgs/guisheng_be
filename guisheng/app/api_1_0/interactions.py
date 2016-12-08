9a6c26c7de78a32cd2ebc1e6ac8c8d3864b45ab9
roseou <598959770@qq.com>
2016-12-08 23:27:36 +0800

    edit command_interactions

--- guisheng/app/api_1_0/interactions.py
+++ guisheng/app/api_1_0/interactions.py
@@ -15,25 +15,29 @@ def get_interaction(id):
         "time":interaction.time.strftime('%m/%d/%Y'),
         "body":interaction.body,
         }),mimetype='application/json')
-    
+ 
 
 @api.route('/interactions/',methods=['GET','POST'])
 def command_interactions():
     interact_id = int(request.get_json().get('article_id'))
     now_interact = Interaction.query.get_or_404(interact_id)
-    tag_id = now_interact.tag[0].tag_id
-    tag = Tag.query.get_or_404(tag_id)
-    interactions = []
-    for _interaction in tag.interactions:
-        interactions.append(_interaction.interaction_id)
-    sortlist = sorted(interactions, key=lambda id: Interaction.query.get_or_404(id).views,reverse=True)
-    command_interactions = sortlist[:3]
+    try:
+        tag_id = now_interact.tag[0].tag_id
+        tag = Tag.query.get_or_404(tag_id)
+        interactions = []
+        for _interaction in tag.interactions:
+            interactions.append(_interaction.interaction_id)
+        sortlist = sorted(interactions, key=lambda id: Interaction.query.get_or_404(id).views,reverse=True)
+        command_interactions = sortlist[:3] if len(sortlist)>=4 else sortlist
+    except:
+        command_interactions = []
     return Response(json.dumps([{
-            "title":interaction.title,
-            "description":interaction.description,
-            "author":User.query.get_or_404(interaction.author_id).name,
+            "title":Interaction.query.filter_by(id=interaction_id).first().title,
+            "description":Interaction.query.filter_by(id=interaction_id).first().description,
+            "author":User.query.get_or_404(Interaction.query.filter_by(id=interaction_id).first().author_id).name,
             "tag":tag.body,
-            "views":interaction.views
-        }for interaction in command_interactions]
+            "views":Interaction.query.filter_by(id=interaction_id).first().views
+        }for interaction_id in command_interactions]
     ),mimetype='application/json')
+
 
