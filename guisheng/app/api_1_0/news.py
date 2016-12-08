fe9043d5871ac99f4e78647591aa6c7d44c3bc49
roseou <598959770@qq.com>
2016-12-08 23:28:50 +0800

    edit command_news

--- guisheng/app/api_1_0/news.py
+++ guisheng/app/api_1_0/news.py
@@ -14,25 +14,28 @@ def get_news(id):
         "time":news.time.strftime('%m/%d/%Y'),
         "body":news.body,
         }),mimetype='application/json')
-    
 
-@api.route('/news', methods=['GET','POST'])
+
+@api.route('/news/', methods=['GET','POST'])
 def command_news():
     news_id = int(request.get_json().get('article_id'))
     now_news = News.query.get_or_404(news_id)
-    tag_id = now_news.tag[0].tag_id
-    tag = Tag.query.get_or_404(tag_id)
-    news = []
-    for _news in tag.news:
-        news.append(_news.news_id)
-    sortlist = sorted(news,key=lambda id: News.query.get_or_404(id).views,reverse=True)
-    command_news = sortlist[:3]
+    try:
+        tag_id = now_news.tag[0].tag_id
+        tag = Tag.query.get_or_404(tag_id)
+        news = []
+        for _news in tag.news:
+            news.append(_news.news_id)
+        sortlist = sorted(news,key=lambda id: News.query.get_or_404(id).views,reverse=True)
+        command_news = sortlist[:3] if len(sortlist)>=4 else sortlist
+    except:
+        command_news=[]
     return Response(json.dumps([{
-            "title":News.query.get_or_404(news_id).title,
-            "description":News.query.get_or_404(news_id).description,
-            "author":User.query.get_or_404(News.query.get_or_404(news_id).author_id).name,
+            "title":News.query.filter_by(id=news_id).first().title,
+            "description":News.query.filter_by(id=news_id).first().description,
+            "author":User.query.get_or_404(News.query.filter_by(id=news_id).first().author_id).name,
             "tag":tag.body,
-             "views":News.query.get_or_404(news_id).views
+            "views":News.query.filter_by(id=news_id).first().views
         }for news_id in command_news]
     ),mimetype='application/json')
 
