21301e72f5d815cb5c49f29f955e4bea828bfb95
roseou <598959770@qq.com>
2016-12-11 14:37:20 +0800

    add make_celery

--- guisheng/guisheng_app/__init__.py
+++ guisheng/guisheng_app/__init__.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -11,7 +12,6 @@ import redis
 from celery import Celery
 
 
-app = Flask(__name__)
 """
 config
  -- 'default': DevelopmentConfig
@@ -20,19 +20,54 @@ config
  -- 'production': ProductionConfig
     you can edit this in config.py
 """
-config_name = 'default'
-app.config.from_object(config[config_name])
-config[config_name].init_app(app)
-toolbar = DebugToolbarExtension(app)
-
-db = SQLAlchemy(app)
-moment = Moment(app)
-login_manager = LoginManager(app)
+toolbar = DebugToolbarExtension()
+db = SQLAlchemy()
+moment = Moment()
+login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 #redis site
 rds = redis.StrictRedis(host='localhost', port=6380, db=0)
+
+'''
+#to search
+from .models import News,Article,Picture,Interaction,Tag
+whooshalchemy.whoosh_index(app, News)
+whooshalchemy.whoosh_index(app, Article)
+whooshalchemy.whoosh_index(app, Picture)
+whooshalchemy.whoosh_index(app, Interaction)
+whooshalchemy.whoosh_index(app, Tag)
+'''
+
+
+def create_app(config_name=None, main=True):
+    if config_name is None:
+        config_name = 'default'
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    config[config_name].init_app(app)
+    db.init_app(app)
+    moment.init_app(app)
+    toolbar.init_app(app)
+    login_manager.init_app(app)
+
+    # admin site
+    from admin import views
+
+    from main import main
+    app.register_blueprint(main, url_prefix='/main')
+
+    from auth import auth
+    app.register_blueprint(auth, url_prefix="/auth")
+
+    from api_1_0 import api
+    app.register_blueprint(api, url_prefix="/api/v1.0")
+
+    return app
+
+app = create_app(config_name = 'default')
 
 #to search
 from .models import News,Article,Picture,Interaction,Tag
@@ -41,25 +76,6 @@ whooshalchemy.whoosh_index(app, Article)
 whooshalchemy.whoosh_index(app, Picture)
 whooshalchemy.whoosh_index(app, Interaction)
 whooshalchemy.whoosh_index(app, Tag)
-
-# admin site
-from admin import views
-
-
-"""
-blueprint
-you can register a <blueprint> by run:
- -- mana blueprint <blueprint>
-under app folder
-"""
-from main import main
-app.register_blueprint(main, url_prefix='/main')
-
-from auth import auth
-app.register_blueprint(auth, url_prefix="/auth")
-
-from api_1_0 import api
-app.register_blueprint(api, url_prefix="/api/v1.0")
 
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
