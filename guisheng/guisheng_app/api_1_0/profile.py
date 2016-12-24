3682b1d9a57352223b94d080a082cf89cf6b7103
roseou <598959770@qq.com>
2016-12-25 00:27:06 +0800

    edit profile.py

--- guisheng/guisheng_app/api_1_0/profile.py
+++ guisheng/guisheng_app/api_1_0/profile.py
@@ -5,25 +5,19 @@ from ..models import Role,User,News,Picture,Article,Interaction,Everydaypic,\
         Collect,Like,Light,Comment
 from . import api
 
+@api.route('/profile/<int:id>/',methods=['GET'])
+def get_profile(id):
+    user=User.query.get_or_404(id)
+    return Response(json.dumps({
+        "img_url":user.img_url,
+        "bg_url":user.bg_url,
+        "name":user.name,
+        "weibo":user.weibo,
+        "introduction":user.introduction
+    }),mimetype='application/json')
 
-@api.route('/profile/<int:id>',methods=['GET','POST','PUT'])
-def profile(id):
-    if request.method == 'GET': 
-        user= User.query.get_or_404(id)
-        return Response(json.dumps({
-            "img_url":user.img_url,
-            "bg_url":user.bg_url,
-            "name":user.name,
-            "weibo":user.weibo,
-            "introduction":user.introduction,
-            "suggestion":user.suggestion,
-            #            "collection":','.join([col.title for col in user.collection.all()]),
-            "works":','.join([inews.title for inews in user.news.all()])+
-                    ','.join([pic.title for pic in user.pictures.all()])+
-                    ','.join([article.title for article in user.articles.all()])+
-                    ','.join([interaction.title for interaction in user.interactions.all()]),
-        }),mimetype='application/json')
-
+@api.route('/profile/<int:id>/edit/',methods=['GET','PUT'])
+def edit_profile(id):
     if request.method == 'PUT':
         user = User.query.get_or_404(id)
         user.img_url = request.get_json().get("img_url")
@@ -31,7 +25,6 @@ def profile(id):
         user.name = request.get_json().get("name")
         user.weibo = request.get_json().get("weibo")
         user.ntroduction = request.get_json().get("introduction")
-        user.suggestion = request.get_json().get("suggestion")
         db.session.add(user)
         db.session.commit()
         return Response(json.dumps({
@@ -40,11 +33,54 @@ def profile(id):
             "name":user.name,
             "weibo":user.weibo,
             "introduction":user.introduction,
-            "works":','.join([inews.title for inews in user.news.all()])+
-                    ','.join([pic.title for pic in user.pictures.all()])+
-                    ','.join([article.title for article in user.articles.all()])+
-                    ','.join([interaction.title for interaction in user.interactions.all()]),
-                    #   "collection":''.join([col.title for col in user.collection.all()]),
-            "suggestion":user.suggestion,
             }),mimetype='application/json')
- 
+
+@api.route('/profile/<int:id>/works/',methods=['GET'])
+def get_works(id):
+    user= User.query.get_or_404(id)
+    articles = [for article in user.articles.all()]
+    articles.sort(key=attrgetter('time'),reverse=True)
+    return Response(json.dumps([{
+            "article_id":_article.id,
+            "img_url":_article.img_url[0],
+            "title":_article.title,
+            "author":user.name,
+            "views":_article.views,
+            "tag":Tag.query.get_or_404(Article.query.get_or_404(_article.id).tag[0].tag_id).body\
+                  if len([i for i in Article.query.get_or_404(_article.id).tag]) else "",
+            "description":_article.description,
+        }for _article in articles]
+    ),mimetype='application/json')
+
+
+@api.route('/profile/<int:id>/collections/',methods=['GET'])
+def get_collections(id):
+    user= User.query.get_or_404(id)
+    alist = []
+    for _collection in Collect.query.filter_by(author_id=id).all():
+        if News.query.get_or_404(_collection.news_id):
+            news = News.query.get_or_404(_collection.news_id)
+            alist.append(news)
+        elif Article.query.get_or_404(_collection.article_id):
+            article = Article.query.get_or_404(_collection.article_id)
+            alist.append(article)
+        elif Picture.query.get_or_404(_collection.picture_id):
+            picture = Picture.query.get_or_404(_collection.picture_id)
+            alist.append(picture)
+        elif Interaction.query.get_or_404(_collection.interaction_id):
+            interaction = Interaction.query.get_or_404(_collection.interaction_id)
+            alist.append(interaction)
+    alist.sort(key=attrgetter('time'),reverse=True)
+    return Response(json.dumps([{
+            "article_id":content.id,
+            "img_url":content.img_url[0],
+            "title":content.title,
+            "author":user.name,
+            "views":content.views,
+            "tag":Tag.query.get_or_404(content.__class__.query.get_or_404(content.id).tag[0].tag_id).body\
+                  if len([i for i in content.__class__.query.get_or_404(content.id).tag]) else "",
+            "description":content.description,
+        }for content in alist]
+    ),mimetype='application/json')
+
+
