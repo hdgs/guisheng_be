21dcbfeb0755c27d5a271c666f61666d0a8ed9d8
roseou <598959770@qq.com>
2017-02-09 10:38:41 +0800

    add like.py

--- guisheng/guisheng_app/api_1_0/light.py
+++ guisheng/guisheng_app/api_1_0/light.py
@@ -6,20 +6,21 @@ from . import api
 
 @api.route('/light/',methods=['GET','POST'])
 def light():
-    light = Light()
-    light.like_degree = int(request.get_json().get("like_degree"))
-    kind = int(request.get_json().get("kind"))
-    if kind == 1:
-        light.news_id = request.get_json().get("article_id") 
-    if kind == 2:
-        light.picture_id = request.get_json().get("article_id")
-    if kind == 3:
-        light.article_id = request.get_json().get("article_id")
-    if kind == 4:
-        light.interaction_id = request.get_json().get("article_id")
-    db.session.add(light)
-    db.session.commit()
-    return jsonify({
-        "status":200
-    })
+    if requset.method == 'POST':
+        light = Light()
+        light.like_degree = int(request.get_json().get("like_degree"))
+        kind = int(request.get_json().get("kind"))
+        if kind == 1:
+            light.news_id = request.get_json().get("article_id") 
+        if kind == 2:
+            light.picture_id = request.get_json().get("article_id")
+        if kind == 3:
+            light.article_id = request.get_json().get("article_id")
+        if kind == 4:
+            light.interaction_id = request.get_json().get("article_id")
+        db.session.add(light)
+        db.session.commit()
+        return jsonify({
+            "status":200
+        })
 
--- guisheng/guisheng_app/api_1_0/like.py
+++ guisheng/guisheng_app/api_1_0/like.py
@@ -1,7 +1,27 @@
 # coding: utf-8
 from flask import render_template,jsonify,Response,g,request
 import json
-from ..models import Light
+from ..models import Like
 from . import api
+from .. import db
 
+@api.route('/like/picture/', methods=['GET','POST'])
+def like_picture():
+    if request.method == 'POST':
+        like = Like()
+        like.picture_id = int(request.get_json().get('picture_id'))
+        db.session.add(like)
+        db.session.commit()
+        return jsonify({
+            'status':200
+        })
 
+@api.route('/like/comment/', methods=['GET','POST'])
+    if request.method == 'POST':
+        like = Like()
+        like.comment_id = int(request.get_json().get('comment_id'))
+        db.session.add(like)
+        db.session.commit()
+        return jsonify({
+            'status':200
+        })
