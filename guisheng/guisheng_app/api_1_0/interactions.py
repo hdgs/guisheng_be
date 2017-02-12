c3ff945b950087dc30985da6ed9a507015795887
roseou <598959770@qq.com>
2017-02-12 09:39:41 +0800

    edit

--- guisheng/guisheng_app/api_1_0/interactions.py
+++ guisheng/guisheng_app/api_1_0/interactions.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from flask import render_template,jsonify,Response,g,request
+from flask_login import current_user
 import json
 from ..models import Role,User,Interaction,Tag,PostTag
 from . import api
@@ -11,7 +12,7 @@ def get_interaction(id):
     like_degree_one = interaction.light.filter_by(like_degree=0).count()
     like_degree_two = interaction.light.filter_by(like_degree=1).count()
     like_degree_three = interaction.light.filter_by(like_degree=2).count()
-    user_role = -1 if g.current_user.is_anonymous else 0
+    user_role = -1 if current_user.is_anonymous else 0
     interaction.views+=1
     db.session.commit()
     return Response(json.dumps({
