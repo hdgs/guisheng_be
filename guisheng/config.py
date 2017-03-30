cbba8ebdf4720fa86efdbd85fcdadae9151e17f8
roseou <598959770@qq.com>
2017-03-30 09:09:58 +0800

    edit

--- guisheng/config.py
+++ guisheng/config.py
@@ -57,8 +57,8 @@ development configuration
 class DevelopmentConfig(Config):
     """development configuration"""
     DEBUG = True
-#    SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URI')
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URI')
+#    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
 
 
 # production configuration
