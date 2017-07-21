f7ae82cf0198662897024c555a78a5c403c48ae6
Humbertzhang <504490160@qq.com>
2017-07-21 22:51:52 +0800

    Add spprojects in __init__

--- guisheng/guisheng_app/api_1_0/__init__.py
+++ guisheng/guisheng_app/api_1_0/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 api = Blueprint('api',__name__)
 
 from . import feed,everydaypic,pictures,news,articles,\
-        interactions,profile,comments,light,authentication,user,collect,like
+        interactions,profile,comments,light,authentication,user,collect,like,spprojects
