028bd4875025b11e05eef1e34c03e774e2632021
Humbertzhang <504490160@qq.com>
2017-08-16 15:52:46 +0800

    Update everydaypic.py

--- guisheng/guisheng_app/api_1_0/everydaypic.py
+++ guisheng/guisheng_app/api_1_0/everydaypic.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask import render_template,jsonify,Response,g,request
 import json
-from  datetime import datetime
+from  datetime import datetime,timedelta
 from ..models import Everydaypic
 from . import api
 from .. import db
