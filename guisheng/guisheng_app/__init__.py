5ea274e9ab9419e31044a351d4ceb2b2f1cef2b1
roseou <598959770@qq.com>
2017-08-18 09:53:28 +0800

    debug

--- guisheng/guisheng_app/__init__.py
+++ guisheng/guisheng_app/__init__.py
@@ -45,13 +45,6 @@ def create_app(config_name=None, main=True):
 
     #to search
     from .models import News,Article,Picture,Interaction,Tag
-    '''
-    whooshalchemy.whoosh_index(app, News)
-    whooshalchemy.whoosh_index(app, Article)
-    whooshalchemy.whoosh_index(app, Picture)
-    whooshalchemy.whoosh_index(app, Interaction)
-    whooshalchemy.whoosh_index(app, Tag)
-    '''
     try:
         whooshalchemy.whoosh_index(app, News)
     except NameError:
@@ -88,47 +81,6 @@ def create_app(config_name=None, main=True):
     return app
 
 app = create_app(config_name = 'default')
-'''
-#to search
-from .models import News,Article,Picture,Interaction,Tag
-whooshalchemy.whoosh_index(app, News)
-whooshalchemy.whoosh_index(app, Article)
-whooshalchemy.whoosh_index(app, Picture)
-whooshalchemy.whoosh_index(app, Interaction)
-whooshalchemy.whoosh_index(app, Tag)
-'''
-'''
-if not os.path.exists('./whoosh_index/News'):
-    print "init"
-    whooshalchemy.whoosh_index(app, News)
-if not os.path.exists('./whoosh_index/Article'):
-    print "init"
-    whooshalchemy.whoosh_index(app, Article)
-if not os.path.exists('./whoosh_index/Picture'):
-    print "init"
-    whooshalchemy.whoosh_index(app, Picture)
-if not os.path.exists('./whoosh_index/Interaction'):
-    print "init"
-    whooshalchemy.whoosh_index(app, Interaction)
-if not os.path.exists('./whoosh_index/Tag'):
-    print "init"
-    whooshalchemy.whoosh_index(app, Tag)
-'''
-'''
-def create_index(app, model):
-    if not hasattr(app, 'whoosh_indexes'):
-        app.whoosh_indexes = {}
-    if model.__name__ in app.whoosh_indexes:
-        return app.whoosh_indexes[model.__name__]
-    else:
-        return app.whoosh_indexes.get(model.__name__,whooshalchemy._create_index(app, model))
-
-create_index(app,News)
-create_index(app,Article)
-create_index(app,Picture)
-create_index(app,Interaction)
-create_index(app,Tag)
-'''
 
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
